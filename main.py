35282383f1011c72d22e3cdefa6c7ece72868d38
cakesmack <cmack6189@gmail.com>
2023-02-15 00:17:32 +0000

    create basic PDF file for each XLSX invoice file

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 filepaths = glob.glob("*xlsx")
-print(filepaths)
 
 for i in filepaths:
     df = pd.read_excel(i, sheet_name='Sheet 1')
-    print(df)
+    pdf = FPDF(orientation='p', unit='mm', format='A4')
+    pdf.add_page()
+    pdf.set_font(family='Times', size=16, style='B')
+    filename = Path(i).stem
+    invoice_no = filename.split('-')[0]
+    print(invoice_no)
+    pdf.cell(w=50, h=8, txt=f'Invoice No. {invoice_no}')
+    pdf.output(f'PDFs/{filename}.pdf')
